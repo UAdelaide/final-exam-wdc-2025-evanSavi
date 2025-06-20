fd9c5da5edbfbeda9b34c4c78b860131d8bed47a
evanSavi <a1927832@adelaide.edu.au>
2025-06-20 10:46:14 +0000

    [automatic commit] 2025-06-20 10:46:14 UTC 58.174.9.9

--- part1/app.js
+++ part1/app.js
@@ -32,9 +32,9 @@ async function insertTestData() {
 
     // Insert walk requests
     await db.execute(`INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
-         ((SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')), 
+         ((SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')),
           '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
-         ((SELECT dog_id FROM Dogs WHERE name = 'Bella' AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')), 
+         ((SELECT dog_id FROM Dogs WHERE name = 'Bella' AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')),
           '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
          ((SELECT dog_id FROM Dogs WHERE name = 'Rocky' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')), 
           '2025-06-11 10:00:00', 60, 'Greenhill Park', 'open'),
