589ee93aa8ffa27b6bd97d5538e5fae19e4e8c22
evanSavi <a1927832@adelaide.edu.au>
2025-06-20 10:33:05 +0000

    [automatic commit] 2025-06-20 10:33:05 UTC 58.174.9.9

--- part1/app.js
+++ part1/app.js
@@ -16,12 +16,12 @@ async function insertTestData() {
 
     // Insert users
     await db.execute(`INSERT INTO Users (username, email, password_hash, role)
-VALUES 
-('alice123', 'alice@example.com', 'hashed123', 'owner'),
-('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-('carol123', 'carol@example.com', 'hashed789', 'owner'),
-('davidwalker', 'david@example.com', 'hashedabc', 'walker'),
-('emilyowner', 'emily@example.com', 'hashedxyz', 'owner');`);
+	VALUES 
+	('alice123', 'alice@example.com', 'hashed123', 'owner'),
+	('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+	('carol123', 'carol@example.com', 'hashed789', 'owner'),
+	('davidwalker', 'david@example.com', 'hashedabc', 'walker'),
+	('emilyowner', 'emily@example.com', 'hashedxyz', 'owner');`);
 
     // Insert dogs
     await db.execute(`INSERT INTO Dogs (owner_id, name, size) VALUES
