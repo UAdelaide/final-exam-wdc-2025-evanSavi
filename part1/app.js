e26fe06bb9739b9a6ad49621e70f1dc1d6c9b6f8
evanSavi <a1927832@adelaide.edu.au>
2025-06-20 10:33:25 +0000

    [automatic commit] 2025-06-20 10:33:25 UTC 58.174.9.9

--- part1/app.js
+++ part1/app.js
@@ -16,7 +16,7 @@ async function insertTestData() {
 
     // Insert users
     await db.execute(`INSERT INTO Users (username, email, password_hash, role)
-	VALUES 
+	VALUES
 	('alice123', 'alice@example.com', 'hashed123', 'owner'),
 	('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
 	('carol123', 'carol@example.com', 'hashed789', 'owner'),
