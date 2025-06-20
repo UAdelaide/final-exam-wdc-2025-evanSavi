a86a9bfa302e2a8414b9f99e90b9bc3bfc3db879
evanSavi <a1927832@adelaide.edu.au>
2025-06-20 10:49:58 +0000

    [automatic commit] 2025-06-20 10:49:58 UTC 58.174.9.9

--- part1/app.js
+++ part1/app.js
@@ -44,9 +44,7 @@ async function insertTestData() {
          '2025-06-13 07:45:00', 50, 'Sunnyvale Street', 'completed')`);
 
     // Insert ratings
-    await db.execute(`
-  INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments)
-  VALUES (
+    await db.execute(`INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments) VALUES (
     (SELECT request_id FROM WalkRequests WHERE dog_id = (SELECT dog_id FROM Dogs WHERE name = 'Bella') LIMIT 1),
     (SELECT user_id FROM Users WHERE username = 'bobwalker'),
     (SELECT user_id FROM Users WHERE username = 'carol123'),
